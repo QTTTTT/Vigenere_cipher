787d67a7e3324bb8d43094a635eba4bb22e9b25c
mac <mac@cernet2.net>
2018-02-27 18:32:22 +0800

    Initial Commit

---
+++ lab1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lab1
+//
+//  Created by mac on 2018/2/27.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
